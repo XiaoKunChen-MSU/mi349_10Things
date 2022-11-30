3f003c776292e8f9fd5805d4a962f791943b689a
XiaoKun Chen <chenx137@msu.edu>
2022-11-29 23:11:08 -0500

    changes

---
+++ 10things.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let math = prompt ("123 * 321 = ?");
+    if (math == 39483) {alert ("Good job! You are so smart!");
+    } else if (math == ""){
+        alert ("Hey, you should put something in the box!")
+    } else {
+        alert ("Come on, you can do it! Try it again.")
+    } 
+}
+
